915c39a07cd2e5099d038ae150f199551c4ff06e
Grady Fitzpatrick <gradyfitz@gmail.com>
2020-05-23 18:04:02 +1000

    Corrected handling of 100% accuracies and added 2b file notification.

--- ass2/verify_ass2_2a.py
+++ ass2/verify_ass2_2a.py
@@ -9,13 +9,13 @@
 #    The second line may be escaped with quotes, as you might expect and spaces
 #        will be ignored.
 # 4. That your program prints the following output:
-#    Accuracy of decision tree: xx.yyy%
-#    Accuracy of k-nn (k=5): xx.yyy%
-#    Accuracy of k-nn (k=10): xx.yyy%
+#    Accuracy of decision tree: xxx.yyy%
+#    Accuracy of k-nn (k=5): xxx.yyy%
+#    Accuracy of k-nn (k=10): xxx.yyy%
 # 5. That the verification script is able to extract these from your output correctly:
-#    Decision tree accuracy: xx.yyy%
-#    k-nn (k=5)  accuracy: xx.yyy%
-#    k-nn (k=10) accuracy: xx.yyy%
+#    Decision tree accuracy: xxx.yyy%
+#    k-nn (k=5)  accuracy: xxx.yyy%
+#    k-nn (k=10) accuracy: xxx.yyy%
 #
 #    NOTE: The correctness of these measures won't be evaluated by this testing
 #          script.
@@ -48,9 +48,9 @@ student_output_name = "task2a.csv"
 pattern_multiple_spaces = r" +"
 pattern_multiple_spaces_repl = r" "
 
-accuracy_dt_re    = r'Accuracy of decision tree:\s*(\d{0,2}(\.\d{0,3})?)'
-accuracy_knn5_re  = r'Accuracy of k-nn \(k=5\):\s*(\d{0,2}(\.\d{0,3})?)'
-accuracy_knn10_re = r'Accuracy of k-nn \(k=10\):\s*(\d{0,2}(\.\d{0,3})?)'
+accuracy_dt_re    = r'Accuracy of decision tree:\s*(\d{0,3}(\.\d{0,3})?)'
+accuracy_knn5_re  = r'Accuracy of k-nn \(k=5\):\s*(\d{0,3}(\.\d{0,3})?)'
+accuracy_knn10_re = r'Accuracy of k-nn \(k=10\):\s*(\d{0,3}(\.\d{0,3})?)'
 
 def clean_string(string: str) -> str:
     cstring = string
@@ -78,7 +78,7 @@ def get_cleaned_dicts(df: pd.DataFrame) -> Dict[str, str]:
 
 
 try:
-    timelog("Running program ({})".format(python_file_name))
+    timelog("Running program ({}).".format(python_file_name))
     start_time = datetime.datetime.now()
     # Run program
     program = subprocess.Popen(['python', '{}'.format(python_file_name)],
@@ -96,22 +96,22 @@ try:
         (end_time - start_time).total_seconds()))
 
     # Check file created correctly.
-    timelog("Loading {}".format(student_output_name))
+    timelog("Loading {}.".format(student_output_name))
 
     student_df = pd.read_csv("{}".format(student_output_name))
 
-    timelog("Loaded {}".format(student_output_name))
+    timelog("Loaded {}.".format(student_output_name))
 
     # Check all required columns are present.
-    assert clean_string('feature') in [clean_string(col) for col in student_df.columns], "column feature missing"
-    assert clean_string('median') in [clean_string(col) for col in student_df.columns], "column median missing"
-    assert clean_string('mean') in [clean_string(col) for col in student_df.columns], "column mean missing"
-    assert clean_string('variance') in [clean_string(col) for col in student_df.columns], "column variance missing"
-    assert len(student_df.columns) == 4, "Unexpected columns {}".format([col for col in student_df.columns if clean_string(col) not in ['feature', 'median', 'mean', 'variance']])
+    assert clean_string('feature') in [clean_string(col) for col in student_df.columns], "column feature missing."
+    assert clean_string('median') in [clean_string(col) for col in student_df.columns], "column median missing."
+    assert clean_string('mean') in [clean_string(col) for col in student_df.columns], "column mean missing."
+    assert clean_string('variance') in [clean_string(col) for col in student_df.columns], "column variance missing."
+    assert len(student_df.columns) == 4, "Unexpected columns {}.".format([col for col in student_df.columns if clean_string(col) not in ['feature', 'median', 'mean', 'variance']])
 
     timelog("Performing basic checking on datasets.")
     student_rows, student_columns = student_df.shape
-    assert student_rows >= 2, "Solution missing rows"
+    assert student_rows >= 2, "Solution missing rows."
 
     cleaned_rows_dict = get_cleaned_dicts(student_df)
 
@@ -134,29 +134,29 @@ try:
     else:
         escaped = True
         ate_row = student_df[student_df[cleaned_rows_dict['feature']] == 'Access to electricity, rural (% of rural population) [EG.ELC.ACCS.RU.ZS]']
-        assert len(ate_row) > 0, "Couldn't find row 'Access to electricity, rural (% of rural population) [EG.ELC.ACCS.RU.ZS]'"
+        assert len(ate_row) > 0, "Couldn't find row 'Access to electricity, rural (% of rural population) [EG.ELC.ACCS.RU.ZS]'."
         ate_median = ate_row[cleaned_rows_dict['median']].iloc[0]
         ate_mean = ate_row[cleaned_rows_dict['mean']].iloc[0]
         ate_variance = ate_row[cleaned_rows_dict['variance']].iloc[0]
 
         asped_row = student_df[student_df[cleaned_rows_dict['feature']] == 'Adjusted savings: particulate emission damage (% of GNI) [NY.ADJ.DPEM.GN.ZS]']
-        assert len(asped_row) > 0, "Couldn't find row 'Adjusted savings: particulate emission damage (% of GNI) [NY.ADJ.DPEM.GN.ZS]'"
+        assert len(asped_row) > 0, "Couldn't find row 'Adjusted savings: particulate emission damage (% of GNI) [NY.ADJ.DPEM.GN.ZS]'."
         asped_median = asped_row[cleaned_rows_dict['median']].iloc[0]
         asped_mean = asped_row[cleaned_rows_dict['mean']].iloc[0]
         asped_variance = asped_row[cleaned_rows_dict['variance']].iloc[0]
 
     timelog("Testing script retrieved the following values from {}:".format(student_output_name))
-    print("Found access to electricity median: {}, mean: {}, variance: {}".format(ate_median, ate_mean, ate_variance))
-    print("Found adjusted savings median: {}, mean: {}, variance: {}".format(asped_median, asped_mean, asped_variance))
+    print("Found values for 'access to electricity' - median: {}, mean: {}, variance: {}.".format(ate_median, ate_mean, ate_variance))
+    print("Found values for 'adjusted savings'      - median: {}, mean: {}, variance: {}.".format(asped_median, asped_mean, asped_variance))
 
-    timelog("Checking program output")
+    timelog("Checking program output.")
     dt_match = re.search(accuracy_dt_re, program_output)
     knn5_match = re.search(accuracy_knn5_re, program_output)
     knn10_match = re.search(accuracy_knn10_re, program_output)
 
-    assert dt_match, "Output was missing accuracy of decision tree"
-    assert knn5_match, "Output was missing accuracy of k-nn (k=5)"
-    assert knn10_match, "Output was missing accuracy of k-nn (k=10)"
+    assert dt_match, "Output was missing accuracy of decision tree."
+    assert knn5_match, "Output was missing accuracy of k-nn (k=5)."
+    assert knn10_match, "Output was missing accuracy of k-nn (k=10)."
 
     dt_accuracy = dt_match.group(1)
     knn5_accuracy = knn5_match.group(1)
